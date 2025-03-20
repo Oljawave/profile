a59487424f992ee685e7a4731401d173a2dc0abe
Oljawave <misterolzhas04@gmail.com>
2025-03-20 18:00:17 +0600

    second commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/me/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/profile/' : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
